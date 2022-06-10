724823a1c53f4c5b30dd4bf371964c428e1b5226
Peter Vang <vang_peter06@yahoo.com>
2022-06-10 10:05:15 -0700

    WIP trying to figure out how to make it into a array.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function beepBoop(inputNumber) {
+  const arrayNum = [inputNumber(i)]; 
+    for (let i = 0; i <= inputNumber; i++) {
+      // arrayNum.push(i);
+      console.log(arrayNum);
+    }
+    return arrayNum;
+  
+}
+inputNumber = 5;
