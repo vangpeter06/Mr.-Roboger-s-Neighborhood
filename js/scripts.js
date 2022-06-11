12ce014efa9e200c36d018b36af611f3e1b58a34
Peter Vang <vang_peter06@yahoo.com>
2022-06-10 17:03:42 -0700

    WIP working on defining arrayNumber.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
-// const inputNumber = 5;
-
-function beepBoop(inputNumber) {
-  let arrayNumber = []; 
+// // const input = 5;
+// let input = parseInt($("#input").val());
+// function beepBoop(inputNumber) {
+//   // let arrayNumber = []; 
   
-    for (let i = 0; i <= inputNumber; i += 1) {
-     console.log(inputNumber);
-      arrayNumber.push(i);
+//     for (let i = 0; i <= inputNumber; i += 1) {
+//     //  console.log(input);
+//     //   arrayNumber.push(i);
+//     }
+//     return arrayNumber;
+// console.log(arrayNumber);
+//   }
+  function beepBoop(inputNumber) {
+    for (i = 0; i <= inputNumber; i++) {
+      let arrayNumber = inputNumber - [i];
+      //return newNum
+      console.log(arrayNumber);
     }
-    arrayNumber;
-console.log(arrayNumber);
-  }
   
+  }
   
   
   $(document).ready(function(){
     $("form#counting").submit(function(event) {
       event.preventDefault();
-      const inputNumber = parseInt($("#inputNumber").val());
+      const inputNumber = parseInt($("#input").val());
       
       // const results = result(arrayNum);
       $(("#output")).text(arrayNumber);
