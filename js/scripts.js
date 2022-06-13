e5e2e5f893da185286f0242a186667557f13401b
Peter Vang <vang_peter06@yahoo.com>
2022-06-12 18:56:43 -0700

    fix defining arrayNumber and test one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,17 @@
-// // const input = 5;
-// let input = parseInt($("#input").val());
-// function beepBoop(inputNumber) {
-//   // let arrayNumber = []; 
+function beepBoom(inputNumber) {
+  let arrayNumber = []; 
+  for (let i = 0; i <= inputNumber; i++) {
+    arrayNumber.push(i);
+  };
+  return arrayNumber;
+ };
   
-//     for (let i = 0; i <= inputNumber; i += 1) {
-//     //  console.log(input);
-//     //   arrayNumber.push(i);
-//     }
-//     return arrayNumber;
-// console.log(arrayNumber);
-//   }
-  function beepBoop(inputNumber) {
-    for (i = 0; i <= inputNumber; i++) {
-      let arrayNumber = inputNumber - [i];
-      //return newNum
-      console.log(arrayNumber);
-    }
-  
-  }
-  
-  
-  $(document).ready(function(){
-    $("form#counting").submit(function(event) {
-      event.preventDefault();
-      const inputNumber = parseInt($("#input").val());
-      
-      // const results = result(arrayNum);
-      $(("#output")).text(arrayNumber);
-      // console.log(arrayNumber);
-      // $("#output").text(results);
-    });
+$(document).ready(function(){
+  $("form#counting").submit(function (event) {
+    event.preventDefault();
+    const inputNumber = $("#inputNumber").val(); 
+    const results = beepBoom(inputNumber);
+     $("#output").text(results);
   });
+});
 
